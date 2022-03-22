7be5705469afa23b6c682fb01192e0b842df0989
lejk8104 <lejk8104@github.com>
2022-03-22 05:38:01 -0400

    waymo dataloader test

--- cylinder3D_waymo/train_cylinder_asym.py
+++ cylinder3D_waymo/train_cylinder_asym.py
@@ -159,7 +159,8 @@ def main(args):
 if __name__ == '__main__':
     # Training settings
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-y', '--config_path', default='config/waymo.yaml')
+    #parser.add_argument('-y', '--config_path', default='config/waymo.yaml')
+    parser.add_argument('-y', '--config_path', default='config/semantickitti.yaml')
     args = parser.parse_args()
 
     print(' '.join(sys.argv))
